cfa5356562ff27f148f798a9f0f69f418a2a7c82
Derrick Abankwa <dkabankwa@yahoo.com>
2021-11-18 02:55:09 -0800

    test deploy

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,6 +5,12 @@ cd /home/ec2-user/social-backend
 #install node modules
 npm ci
 
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ]  && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 #reload app
 cd /home/ec2-user/social-backend/dist/src
 pm2 start app.js --name backend
