c4cd20366f8e18604fd7ef76f5378b266275f55d
Derrick Abankwa <dkabankwa@yahoo.com>
2021-11-18 10:43:45 -0800

    test deploy

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,6 +7,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ]  && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+sudo chown ec2-user /home/ec2-user/social-backend
+sudo chmod -R 777 /home/ec2-user/social-backend
+
 #install node modules
 npm ci
 
